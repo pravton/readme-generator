ce1fb3d159357e11f1fab3fa687adf55ec50d973
PravTon <clintonsf@outlook.com>
2021-06-13 18:16:26 -0400

    install inquirer and setup

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer
+    .prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+    .then(answers => {
+        console.log(answers);
+    });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license) {
+    return 'https://img.shields.io/badge/licence-' + license + '-green';
+  } else {
+    console.log('No license provided');
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,7 +20,6 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
 `;
 }
 
