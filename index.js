c71393a5a9319c7340ff5b7ddf5fdac40bc045c8
PravTon <clintonsf@outlook.com>
2021-06-14 11:41:58 -0400

    updated questions

--- index.js
+++ index.js
@@ -4,8 +4,214 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is your project name?',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the project name!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description for your project?',
+        validate: descInput => {
+            if(descInput) {
+                return true;
+            } else {
+                console.log('Please enter a description!')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'tableOfContents',
+        message: 'Select your table of contents',
+        choices: ['Installation', 'Usage', 'Credits', 'License']
+    }, 
+    {
+        type: 'confirm',
+        name: 'confirmInstallation',
+        message: 'Would you like to enter some information about installation?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'aboutInstallation',
+        message: 'Provide some information about installation:',
+        when: ({confirmInstallation}) => {
+            if (confirmInstallation) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'confirm',
+        name: 'confirmUsage',
+        message: 'Would you like to enter some information about usage?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'aboutUsage',
+        message: 'Provide some information about usage:',
+        when: ({confirmUsage}) => {
+            if (confirmUsage) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLicense',
+        message: 'Would you like to include a license?',
+        default: false
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select one of the license:',
+        choices: [{
+            name: 'GNU AGPLv3',
+            value: 'AGPL-3.0'
+        },
+        {
+            name: 'GNU GPLv3',
+            value: 'GPL-3.0'
+        },
+        {
+            name: 'GNU LGPLv3',
+            value: 'LGPL-3.0'
+        },
+        {
+            name: 'Mozilla Public License 2.0',
+            value: 'MPL-2.0'
+        },
+        {
+            name: 'Apache License 2.0',
+            value: 'Apache-2.0'
+        },
+        {
+            name: 'MIT License',
+            value: 'MIT'
+        },
+        {
+            name: 'Boost Software License 1.0',
+            value: 'BSL-1.0' 
+        },
+        {
+            name: 'The Unlicense',
+            value: 'Unlicense'
+        }],        
+        when: ({confirmLicense}) => {
+            if (confirmLicense) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContributor',
+        message: 'Would you like to enter some information about contributos?',
+        default: false
+    },
+    {
+        type: 'input',
+        name: 'aboutContributor',
+        message: 'Provide some information about contributors:',
+        when: ({confirmContributor}) => {
+            if (confirmContributor) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'confirm',
+        name: 'confirmTests',
+        message: 'Would you like to enter some information about tests?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'aboutTests',
+        message: 'Provide some information about tests:',
+        when: ({confirmTests}) => {
+            if (confirmTests) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'confirm',
+        name: 'confirmQuestions',
+        message: 'Would you like to enter your contact information for questions?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: emailInput => {
+            if(emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address')
+                return false;
+            }
+        },
+        when: ({confirmQuestions}) => {
+            if (confirmQuestions) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your email address?',
+        validate: githubInput => {
+            if(githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your github Username')
+                return false;
+            }
+        },
+        when: ({confirmQuestions}) => {
+            if (confirmQuestions) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 ];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
   if(license) {
     return 'https://img.shields.io/badge/licence-' + license + '-green';
@@ -11,15 +12,28 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license) {
+    return 'https://choosealicense.com/licenses/' + license.toLowerCase() + '/';
+  } else {
+    console.log('No license provided');
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license) {
+    return 'This application is covered under the ['+ license +'] (' + renderLicenseLink(license) + ') license.'
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectName}
+
+  ${data.description}
 `;
 }
 
